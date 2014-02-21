e57ecc0a11f784e0bc327bbeeffe1227091395d3
836131325@qq.com <ambow411@>
2014-02-21 12:08:28 +0800

    js 对象和应用

--- Day15/Example3.java
+++ Day15/Example3.java
@@ -32,3 +32,100 @@ public class Example3 {
 	}
 
 }
+
+伪代码：程序员分析的过程，半文字、半代码的方式编写的代码。
+看上去像，但是不能运行。
+
+
+定义一个int型的一维数组，包含40个元素，用来存储每个学员的成绩，
+循环产生40个 0~100之间的随机整数，将它们存储到一维数组中，
+然后统计成绩低于平均分的学员的 人数，并输出出来。
+
+(1)定义一个数组
+   var arr = new Array();
+   var arr = [];
+(2)存放 40 个学员成绩,确保数组中有 40 个值
+   里面全部存入 0
+   for(var i=0;i<40;i++){
+	arr[i] = 0;
+   }
+   
+   for(var i=0;i<40;arr[i++]=0) ;
+(3)产生随机数
+   var num = Math.round((Math.random()*100));
+
+(4)把 (2)(3)两个步骤合并
+  for(var i=0;i<40;i++){
+        var num = Math.round((Math.random()*100));
+	arr[i] = num;
+   }
+(5)根据 40 个成绩，得出平均数
+   var ave = 0;    //平均数
+   var sum = 0;    //和
+   for(var i=0;i<40;i++){
+      //每次循环，从数组中取出一个值
+      var a = arr[i];
+      //把每次循环得到的值相加，求和
+      sum = sum + a;
+   }
+   //代码运行到这里，sum 是和
+   avg = sum / arr.length;
+(6)把每个人的成绩与平均数比较，小的打印
+   var person_number = 0;  //保存人数值
+
+   for(var i=0;i<40;i++){
+	var a = arr[i]; //每个人的成绩
+        //跟平均成绩比较
+        if(a < avg){
+             //统计人数值
+             person_number ++; 
+             //打印输出
+             console.log(a);
+        }
+   }   
+
+    //最后，输出小于平均数的值
+    console.log(person_number);
+
+(7)优化代码，合并 (4) (5) 两步
+   var ave = 0;    //平均数
+   var sum = 0;    //和
+   for(var i=0;i<40;i++){
+      var num = Math.round((Math.random()*100));
+      arr[i] = num;
+
+      //把每次循环得到的值相加，求和
+      sum = sum + arr[i];
+   }
+   //代码运行到这里，sum 是和
+   avg = sum / arr.length;
+
+(8) 合并 (6) 代码
+   var ave = 0;    //平均数
+   var sum = 0;    //和
+   var person_number = 0;  //保存人数值
+
+   for(var i=0;i<40;i++){
+      var num = Math.round((Math.random()*100));
+      arr[i] = num;
+
+      //把每次循环得到的值相加，求和
+      sum = sum + arr[i];
+   }
+   //代码运行到这里，sum 是和
+   avg = sum / arr.length;
+    
+   for(var i=0;i<40;i++){
+	var a = arr[i]; //每个人的成绩
+        //跟平均成绩比较
+        if(a < avg){
+             //统计人数值
+             person_number ++; 
+             //打印输出
+             console.log(a);
+        }
+   }   
+
+    //最后，输出小于平均数的值
+    console.log(person_number);
+
---
+++ Day17/my.js
@@ -0,0 +1,6 @@
+//定义自己的函数
+function f2(){
+    for(var i=0;i<3;i++){
+        alert("hello");
+    }
+}
